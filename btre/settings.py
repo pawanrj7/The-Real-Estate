3b8043f88ef2bc24062782826f3171f5a0ceb370
pawanrj7 <pr.pawanrj7@gmail.com>
2020-10-07 18:14:09 +0530

    admin layout is added

--- btre/settings.py
+++ btre/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR,'btre/static')
 ]
+
+#Media Folder setting
+
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_URL= '/media/'
