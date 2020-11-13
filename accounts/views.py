085a068a34125396c2f8f4c73558e1415ccf0b17
pawanrj7 <pr.pawanrj7@gmail.com>
2020-11-13 21:00:28 +0530

    Dashboard page is added

--- accounts/views.py
+++ accounts/views.py
@@ -43,8 +43,17 @@ def register(request):
 
 def login(request):
     if request.method=='POST':
-        #Login User
-        return
+        username= request.POST[username]
+        password= request.POST[password]
+
+        user= auth.authenticate(username=username, password= password)
+
+        if user is not None:
+            messages.success(request, 'You are logged in ')
+            return redirect('dashboard')
+        else:
+            messages.error(request,'Invalid Credentials')
+            return redirect('login')
     else:
         
      return render(request, 'accounts/login.html')
